529df8f75e9971aeb31577df0d09b32aacd4e379
smc0210 <smc0210@naver.com>
2019-09-25 20:26:18 +0900

    login form - hooks

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { gql } from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
+import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <Router isLoggedIn={false} />
-    </>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Wrapper>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+        <Footer />
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
