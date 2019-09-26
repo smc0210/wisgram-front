8dd894c1279656bede0e3f0581ec78bc9e2bf2e0
smc0210 <smc0210@naver.com>
2019-09-26 10:12:15 +0900

    Toastify,  createAccount Mutation 추가

--- src/Routes/Auth/AuthQueries.js
+++ src/Routes/Auth/AuthQueries.js
@@ -1,7 +1,23 @@
-mport { gql } from "apollo-boost";
+import { gql } from "apollo-boost";
 
 export const LOG_IN = gql`
   mutation requestSecret($email: String!) {
     requestSecret(email: $email)
+  }
+`;
+
+export const CREATE_ACCOUNT = gql`
+  mutation createAccount(
+    $username: String!
+    $email: String!
+    $firstName: String
+    $lastName: String
+  ) {
+    createAccount(
+      username: $username
+      email: $email
+      firstName: $firstName
+      lastName: $lastName
+    )
   }
 `;
