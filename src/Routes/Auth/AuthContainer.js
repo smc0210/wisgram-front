52d7197ac003867af9af197a3d9010cb504550fb
smc0210 <smc0210@naver.com>
2019-09-26 11:35:53 +0900

    로그인, 회원가입 마무으리

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import {
+  LOG_IN,
+  CREATE_ACCOUNT,
+  CONFIRM_SECRET,
+  LOCAL_LOG_IN
+} from "./AuthQueries";
 import { toast } from "react-toastify";
 
 export default () => {
@@ -11,7 +16,7 @@ export default () => {
   const firstName = useInput("");
   const lastName = useInput("");
   const secret = useInput("");
-  const email = useInput("smc6711@gmail.com");
+  const email = useInput("");
   const [requestSecretMutation] = useMutation(LOG_IN, {
     variables: { email: email.value }
   });
@@ -23,6 +28,14 @@ export default () => {
       lastName: lastName.value
     }
   });
+
+  const [confirmSecretMutation] = useMutation(CONFIRM_SECRET, {
+    variables: {
+      email: email.value,
+      secret: secret.value
+    }
+  });
+  const [localLogInMutation] = useMutation(LOCAL_LOG_IN);
 
   const onSubmit = async e => {
     e.preventDefault();
@@ -67,6 +80,21 @@ export default () => {
         }
       } else {
         toast.error("모든 field는 필수입니다");
+      }
+    } else if (action === "confirm") {
+      if (secret.value !== "") {
+        try {
+          const {
+            data: { confirmSecret: token }
+          } = await confirmSecretMutation();
+          if (token !== "" && token !== undefined) {
+            localLogInMutation({ variables: { token } });
+          } else {
+            throw Error();
+          }
+        } catch {
+          toast.error("secret요청에 실패했습니다, 재시도 하십시오");
+        }
       }
     }
   };
